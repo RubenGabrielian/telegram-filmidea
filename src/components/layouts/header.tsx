1f15c2018be2f0f329e3ec24f70029f2df9989a0
Ruben.Gabrielyan <Ruben.Gabrielyan@ameriabank.am>
2024-07-24 09:36:59 +0400

    padding

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,7 +1,7 @@
 import logo from "../../assets/logo.svg";
 export default function Header () {
     return (
-        <header className={'mt-5 justify-center flex items-center'}>
+        <header className={'pt-5 justify-center flex items-center'}>
             <img src={logo} alt=""/>
         </header>
     )
