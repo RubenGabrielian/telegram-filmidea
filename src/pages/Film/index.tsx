641bc739b2bebab66b1f09e9a47c366a138aa923
Ruben.Gabrielyan <gabrielianruben@gmail.com>
2025-02-07 15:59:10 +0400

    aa

--- src/pages/Film/index.tsx
+++ src/pages/Film/index.tsx
@@ -2,6 +2,7 @@ import Layout from "../../components/layouts/layout.tsx";
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import FilmView from "../../components/film/Film.tsx";
+import axiosInstance from "../../api/axiosInstance.ts";
 
 const Film = () => {
 
@@ -9,15 +10,13 @@ const Film = () => {
     const [film, setFilm] = useState<any>();
 
     useEffect(() => {
-        fetch(`https://devback.filmidea.tv/api/v1/films/${id}`, {
+        axiosInstance(`https://devback.filmidea.tv/api/v1/films/${id}`, {
             headers: {
                 "X-localization": "ru"
             }
-        }).then((res) => {
-            return res.json()
         }).then((response) => {
-            console.log(response.data);
-            setFilm(response.data);
+            console.log(response.data.data);
+            setFilm(response.data.data);
         })
     }, [id]);
 
