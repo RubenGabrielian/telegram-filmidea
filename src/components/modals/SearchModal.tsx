1e8d5c68b7043b3a20fd36ede451162707d0ba06
Ruben.Gabrielyan <gabrielianruben@gmail.com>
2025-03-06 11:48:25 +0400

    autofocyus

--- src/components/modals/SearchModal.tsx
+++ src/components/modals/SearchModal.tsx
@@ -13,6 +13,7 @@ export default function SearchModal({isOpen, setOpen}: { isOpen: boolean, setOpe
                         <img src={logo} className="mt-5 relative left-[50%] translate-x-[-50%] mb-4" alt=""/>
                         <div className="relative w-[80%] max-w-sm top-3 left-[50%] translate-x-[-50%]">
                             <input type="text" placeholder="Search..."
+                                   autoFocus={true}
                                    className="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-xl bg-transparent text-sm focus:ring-0 focus:border-[#febb12] focus:outline-none"/>
                             <svg className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-500"
                                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
