ee1ead45117ac3550d5d47402e077767bb6a8fd4
Ruben.Gabrielyan <gabrielianruben@gmail.com>
2025-03-12 13:18:29 +0400

    sdsdsd

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
 
+
+//@ts-ignore
 function useDebounce(value: string | undefined, delay: number): string {
+//@ts-ignore
+
     const [debouncedValue, setDebouncedValue] = useState<string>(value);
 
     useEffect(() => {
         const handler = setTimeout(() => {
+//@ts-ignore
+
             setDebouncedValue(value);
         }, delay);
 
